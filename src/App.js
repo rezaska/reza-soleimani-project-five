8f2c8d2e7ca0fbb79f44a0365f02b70f37bf0673
rezaska <reza.soleimani.ka@gmail.com>
2020-11-29 10:45:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import Axios from 'axios';
+import { Component } from 'react';
 import './App.css';
+import Header from './Header.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    //initialize state to keep track of the data that is coming back from the API
+    this.state = {
+      title: '',
+      image: 'https://image.tmdb.org/t/p/original/{TitleImage}',
+      rate: '',
+      overview: '',
+      imdbLink: 'https://www.imdb.com/title/{TitleImdbId}'
+    }
+  }
+
+  componentDidMount() {
+    //make a request to the API once the component has been mounted on the DOM and take the returned data and put it on the page
+    Axios({
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/search/movie?api_key=cd7b67374269e15777a55aee45332dab&query={}',
+      // url: 'https://api.themoviedb.org/3/tv/{tvshowTitle}?api_key=cd7b67374269e15777a55aee45332dab',
+      responseType: 'json',
+      params: {
+        key: 'cd7b67374269e15777a55aee45332dab',
+        q: this.state.title
+      }
+    }).then((apiData) => {
+      console.log(apiData.data.results);
+
+      this.setState({
+        title: apiData.data.results
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+          <Header />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+class Header extends Component {
+
+    render() {
+        return (
+            <header className='mainHeader'>
+                <h1>NextWatch</h1>
+            </header>
+        )
+    }
+
+}
+
+
+
+export default Header;
